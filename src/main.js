a72f677d11dec963ccee17ed9bdd49388753c82f
Mporo <polinamporo@gmail.com>
2026-09-07 15:55:27 +0300

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import './styles/reset.css';
+import Phaser from 'phaser';
+import MainScene from './scenes/MainScene.js';
+
+const config = {
+    type: Phaser.AUTO,
+
+    scale: {
+    mode: Phaser.Scale.RESIZE,
+    width: '100%',
+    height: '100%'
+    },
+
+    backgroundColor: '#0f0f0f',
+
+    scene: [MainScene]
+};
+
+new Phaser.Game(config);

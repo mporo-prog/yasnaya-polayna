71643515a236ea2eb6d01d3dbffd91f57b94505d
LinaSova <Polka.sok@yandex.ru>
2026-09-10 00:22:29 +0300

    Create storyBackgrounds.js

---
+++ resource/data/story/storyBackgrounds.js
@@ -0,0 +1,43 @@
+/**
+ * ССЫЛКИ НА ФОНЫ.
+ *
+ *
+ */
+window.VN.data.storyBackgrounds = [
+  // Сюжетная сцена 1
+  [
+    'assets/backgrounds/scene1_screen1.jpg',
+    'assets/backgrounds/scene1_screen2.jpg',
+    'assets/backgrounds/scene1_screen3.jpg',
+  ],
+  // Сюжетная сцена 2
+  [
+    'assets/backgrounds/scene2_screen1.jpg',
+    'assets/backgrounds/scene2_screen2.jpg',
+    'assets/backgrounds/scene2_screen3.jpg',
+  ],
+  // Сюжетная сцена 3
+  [
+    'assets/backgrounds/scene3_screen1.jpg',
+    'assets/backgrounds/scene3_screen2.jpg',
+    'assets/backgrounds/scene3_screen3.jpg',
+  ],
+  // Сюжетная сцена 4
+  [
+    'assets/backgrounds/scene4_screen1.jpg',
+    'assets/backgrounds/scene4_screen2.jpg',
+    'assets/backgrounds/scene4_screen3.jpg',
+  ],
+  // Сюжетная сцена 5
+  [
+    'assets/backgrounds/scene5_screen1.jpg',
+    'assets/backgrounds/scene5_screen2.jpg',
+    'assets/backgrounds/scene5_screen3.jpg',
+  ],
+  // Сюжетная сцена 6
+  [
+    'assets/backgrounds/scene6_screen1.jpg',
+    'assets/backgrounds/scene6_screen2.jpg',
+    'assets/backgrounds/scene6_screen3.jpg',
+  ],
+];

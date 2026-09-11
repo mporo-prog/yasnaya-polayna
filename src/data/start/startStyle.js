f3893ac663229200b27d67df2d2e7abfafaa9520
LinaSova <Polka.sok@yandex.ru>
2026-09-12 02:10:48 +0300

    Create startStyle.js

---
+++ src/data/start/startStyle.js
@@ -0,0 +1,14 @@
+export const startStyle = {
+  backgroundColor: 0xffffff,
+  panelColor: 0xd9d9d9,
+  textColor: '#000000',
+  titleFontSize: '48px',
+  buttonFontSize: '34px',
+
+  title: { xFrac: 0.5, yFrac: 0.19, wFrac: 0.57, hFrac: 0.24 },
+  buttons: [
+    { xFrac: 0.5, yFrac: 0.48, wFrac: 0.25, hFrac: 0.11 },
+    { xFrac: 0.5, yFrac: 0.645, wFrac: 0.25, hFrac: 0.11 },
+    { xFrac: 0.5, yFrac: 0.81, wFrac: 0.25, hFrac: 0.11 },
+  ],
+};

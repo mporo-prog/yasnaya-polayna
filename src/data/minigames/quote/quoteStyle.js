0cb70b909957185c5bdc7e1535c510871fb92e95
LinaSova <Polka.sok@yandex.ru>
2026-09-12 02:10:36 +0300

    Create quoteStyle.js

---
+++ src/data/minigames/quote/quoteStyle.js
@@ -0,0 +1,13 @@
+export const quoteStyle = {
+  colorDefault: 0xfff5f5,
+  colorWrong: 0xff9999,
+  colorCorrect: 0xccffaa,
+  portraitColor: 0x4d4d4d,
+  backgroundColor: 0xd9d9d9,
+
+  slots: [
+    { xFrac: 0.53, yFrac: 0.46, w: 270, h: 80 },
+    { xFrac: 0.9, yFrac: 0.465, w: 270, h: 80 },
+    { xFrac: 0.71, yFrac: 0.63, w: 420, h: 80 },
+  ],
+};

28de2298b1af81d9d4e0ba5e0b7f9270dcf4b879
LinaSova <Polka.sok@yandex.ru>
2026-09-12 02:10:28 +0300

    Create quoteData.js

---
+++ src/data/minigames/quote/quoteData.js
@@ -0,0 +1,7 @@
+export const quoteData = {
+  characterLabel: 'Толстого',
+  mode: 'suffix',
+  prompt: 'Без любви жить легче.',
+  answer: 'Но без нее нет смысла',
+  distractors: ['Да и фиг с ней', 'Да и фиг с ней'],
+};

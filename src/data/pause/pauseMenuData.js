d5a852b29a53cb3a6f586f4fd99aa0bf5623b4be
LinaSova <Polka.sok@yandex.ru>
2026-09-12 02:10:38 +0300

    Create pauseMenuData.js

---
+++ src/data/pause/pauseMenuData.js
@@ -0,0 +1,7 @@
+export const pauseMenuData = {
+  buttons: [
+    { label: 'ПРОДОЛЖИТЬ', action: 'resume' },
+    { label: 'НАСТРОЙКИ', action: 'settings' },
+    { label: 'МЕНЮ', action: 'menu' },
+  ],
+};

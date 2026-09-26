eee51ef3a359b54eeb0cd82728894b9875db7b06
Mporo <polinamporo@gmail.com>
2026-09-26 17:29:37 +0300

    Change timer

--- games/game4/GameScene4.js
+++ games/game4/GameScene4.js
@@ -220,8 +220,6 @@ export class GameScene4 extends Phaser.Scene {
             this.totalLetters = this.letters.length;
             this.sortedLetters = 0;
             this.timeLeft = 30;
-
-            //
         }
 
         const letterWidth = 350 * this.gameScale;

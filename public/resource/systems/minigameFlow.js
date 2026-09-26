7978f31a24e09db9dff780f1d097e80afb16bcb5
LinaSova <Polka.sok@yandex.ru>
2026-09-26 12:15:44 +0300

    click button at the end return to menu window

--- public/resource/systems/minigameFlow.js
+++ public/resource/systems/minigameFlow.js
@@ -13,14 +13,11 @@
       GameState.goToScreen(nextIndex, 0);
       scene.scene.start('StoryScene', { storySceneIndex: nextIndex, screenIndex: 0 });
     } else {
+    
       //Вызов сброса прогресса
       GameState.reset();
-      scene.add
-        .text(scene.scale.width / 2, scene.scale.height / 2, 'Конец прототипа', {
-          fontSize: '40px',
-          color: '#ffffff',
-        })
-        .setOrigin(0.5);
+      //  кнопка "Завершить" ведёт в главное меню
+      scene.scene.start('MainMenuScene');
     }
   };
 })();
